33755161c8f4f8316822db443ec608bcd65491b5
PietroCok <pietrocok97@gmail.com>
2025-07-30 19:34:59 +0200

    added protection for all routes
    all pages are available once the user logs in or decide to proceed in offline mode (both feature to be implemented in the future)

---
+++ src/components/common/ProtectedRoute.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+import { useAuth } from '../../context/AuthContext';
+import LoadingSpinner from "./LoadingSpinner";
+
+
+/**
+ * Checks if the user is authenticated or is navigating in offline mode
+ */
+export default function ProtectedRoute() {
+  const { isAuthenticated, isOffline, loading } = useAuth();
+
+  if (loading) return (
+    <LoadingSpinner
+      color={"var(--main-color)"}
+      duration={2}
+      radius={40}
+    />
+  )
+
+  if (isAuthenticated || isOffline) {
+    return <Outlet />
+  }
+
+  return <Navigate to="/login" />
+}
