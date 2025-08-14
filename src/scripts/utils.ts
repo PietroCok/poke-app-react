ba0812c75349d79f38f247fc1eddff29fefdffe3
PietroCok <pietrocok97@gmail.com>
2025-08-14 18:41:29 +0200

    added util function to creat item description form object

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,5 +1,8 @@
+import type { AppConfig, Poke } from "@/types";
 
+import appConfig from '../../config.json';
 
+const config: AppConfig = appConfig;
 
 export function ingredientNameToId(name: string){
   if(!name) return '';
@@ -7,10 +10,41 @@ export function ingredientNameToId(name: string){
   return name.replaceAll(" ", "-").replaceAll("'", "--");
 }
 
-
 export function ingredientIdToName(id: string){
   if(!id) return '';
 
   return id.replaceAll("--", "'").replaceAll("-", " ");
 }
 
+/**
+* Converts an item to a string
+* 
+* @retruns a string representing the item
+*/
+export function itemToString(item: Poke) {
+  let str = '';
+  try {
+    str = `${item.size.toUpperCase()}: `;
+    const ingredients = item.ingredients;
+    const groups = Object.keys(ingredients);
+    groups.sort((groupA, groupB) => {
+      const orderA = config.gruppi[groupA].order;
+      const orderB = config.gruppi[groupB].order;
+
+      return orderA - orderB;
+    })
+
+    for (const group of groups) {
+      for (const ingredient of ingredients[group]) {
+        str += ingredient.id.replaceAll("-", " ").replaceAll("--", "'") + (ingredient.quantity > 1 ? " x" + ingredient.quantity : "") + ", ";
+      }
+    }
+
+    // rimozione ultima virgola
+    str = str.slice(0, str.length - 2);
+  } catch (error) {
+    str = 'Error loading item description...'
+  }
+
+  return str;
+}
