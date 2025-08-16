c69586005b7db28c3fc6fe615ac11e5056e67af9
PietroCok <pietrocok97@gmail.com>
2025-08-16 12:23:29 +0200

    added util function to shallow compare objects

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -16,6 +16,19 @@ export function ingredientIdToName(id: string){
   return id.replaceAll("--", "'").replaceAll("-", " ");
 }
 
+// First level compare of objects
+export function shallowEqual(obj1: Record<string, any>, obj2: Record<string, any>) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (const key of keys1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  return true;
+}
+
 /**
 * Converts an item to a string
 * 
