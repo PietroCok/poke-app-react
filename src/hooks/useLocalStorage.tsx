a738c52919ba86cdb76b6f307e4f5bfd150bbeeb
PietroCok <pietrocok97@gmail.com>
2025-08-17 21:18:37 +0200

    added useLocalStorageReducer
    + refactor

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,21 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 
 export function useLocalStorage<T>(
   storageKey: string, 
   initialValue: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [value, setValue] = useState<T>(() => {
-    const storageValue = localStorage.getItem(storageKey);
-    try {
-      return storageValue != null ? JSON.parse(storageValue) as T : initialValue;
-    } catch (error) {
-      return initialValue;      
-    }
-  });
+  const [value, setValue] = useState<T>(() => loadFromLocalStorage(storageKey, initialValue));
 
   useEffect(() => {
     localStorage.setItem(storageKey, JSON.stringify(value));
   }, [storageKey, value])
 
   return [value, setValue];
+}
+
+export function useLocalStorageReducer<T, A>(
+  storageKey: string,
+  reducer: (state: T, action: A) => T,
+  initialValue: T
+): [T, React.Dispatch<A>] {
+  const [value, dispatch] = useReducer(reducer, initialValue, () => loadFromLocalStorage(storageKey, initialValue));
+
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(value));
+  }, [storageKey, value])
+
+  return [value, dispatch];
+}
+
+export function loadFromLocalStorage<T>(key: string, defaultValue: T) {
+  const storageValue = localStorage.getItem(key);
+  try {
+    return storageValue != null ? (JSON.parse(storageValue) as T) : defaultValue;
+  } catch (error) {
+    return defaultValue;
+  }
 }
