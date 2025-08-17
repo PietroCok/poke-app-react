135fc9f0adeac39ae03884764b5bcab0ce7981b4
PietroCok <pietrocok97@gmail.com>
2025-08-17 21:21:27 +0200

    refactor selectionReducer

--- src/context/configurator/selectionReducer.ts
+++ src/context/configurator/selectionReducer.ts
@@ -1,16 +1,19 @@
 import appConfig from '../../../config.json';
 import type { ContextIngredient, IngredientsState } from '@/types';
 
-export type ActionType = typeof ACTIONS[keyof typeof ACTIONS];
+export type ActionType = typeof SELECTION_ACTIONS[keyof typeof SELECTION_ACTIONS];
 
 type Action = {
   type: ActionType,
-  payload?: any
+  groupId?: string,
+  ingredientId?: string
+  ingredient?: ContextIngredient
+  ingredients?: IngredientsState
 }
 
 export const emptyIngredients = Object.fromEntries(Object.entries(appConfig.gruppi).map(group => [group[0], []]));
 
-export const ACTIONS = {
+export const SELECTION_ACTIONS = {
   ADD_INGREDIENT: 'add_ingredient',
   REMOVE_INGREDIENT: 'remove_ingredient',
   INCREMENT_QUANTITY: 'increment_quantity',
@@ -21,38 +24,56 @@ export const ACTIONS = {
 export function selectionReducer(state: IngredientsState, action: Action): IngredientsState {
   switch (action.type) {
 
-    case ACTIONS.ADD_INGREDIENT: {
-      const { group, ingredient } = action.payload;
+    case SELECTION_ACTIONS.ADD_INGREDIENT: {
+      const { groupId, ingredient } = action;
+      if(!groupId || !ingredient){
+        console.error(`One or more parameter is missing for action: ${SELECTION_ACTIONS.ADD_INGREDIENT} on SelectionReducer`);
+         return state;
+      }
+
       const updatedState = {
         ...state,
-        [group]: [...state[group], ingredient]
+        [groupId]: [...state[groupId], ingredient]
       };
       return updatedState;
     }
 
-    case ACTIONS.REMOVE_INGREDIENT: {
-      const { group, ingredientId } = action.payload;
+    case SELECTION_ACTIONS.REMOVE_INGREDIENT: {
+      const { groupId, ingredientId } = action;
+      if(!groupId || !ingredientId) {
+        console.error(`One or more parameter is missing for action: ${SELECTION_ACTIONS.REMOVE_INGREDIENT} on SelectionReducer`);
+        return state;
+      }
+
       const updatedState = {
         ...state,
-        [group]: state[group].filter((ingredient: ContextIngredient) => ingredient.id != ingredientId)
+        [groupId]: state[groupId].filter((ingredient: ContextIngredient) => ingredient.id != ingredientId)
       };
       return updatedState;
     }
 
-    case ACTIONS.INCREMENT_QUANTITY: {
-      const { group, ingredientId } = action.payload;
+    case SELECTION_ACTIONS.INCREMENT_QUANTITY: {
+      const { groupId, ingredientId } = action;
+      if(!groupId || !ingredientId) {
+        console.error(`One or more parameter is missing for action: ${SELECTION_ACTIONS.INCREMENT_QUANTITY} on SelectionReducer`);
+        return state;
+      }
+
       const updatedState = {
         ...state,
-        [group]: state[group].map((ingredient: ContextIngredient) =>
+        [groupId]: state[groupId].map((ingredient: ContextIngredient) =>
           ingredient.id == ingredientId ? { ...ingredient, quantity: ingredient.quantity + 1 } : ingredient
         )
       }
       return updatedState;
     }
 
-    case ACTIONS.SET_INGREDIENTS: {
-      const { ingredients } = action.payload;
-      if(!ingredients) return state;
+    case SELECTION_ACTIONS.SET_INGREDIENTS: {
+      const { ingredients } = action;
+      if(!ingredients){
+        console.error(`One or more parameter is missing for action: ${SELECTION_ACTIONS.SET_INGREDIENTS} on SelectionReducer`);
+        return state;
+      }
 
       return {
         ...emptyIngredients,
@@ -60,7 +81,7 @@ export function selectionReducer(state: IngredientsState, action: Action): Ingre
       }
     }
 
-    case ACTIONS.RESET: {
+    case SELECTION_ACTIONS.RESET: {
       return emptyIngredients;
     }
 
