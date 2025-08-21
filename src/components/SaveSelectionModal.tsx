e39c744e98b0fed1c3ab499d82f573eb6953ddc5
PietroCok <pietrocok97@gmail.com>
2025-08-21 09:36:09 +0200

    save element modal refactor

--- src/components/SaveSelectionModal.tsx
+++ src/components/SaveSelectionModal.tsx
@@ -1,5 +1,4 @@
 import { type ChangeEvent } from "react";
-import { createPortal } from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faStar, faX } from "@fortawesome/free-solid-svg-icons";
 
@@ -10,6 +9,7 @@ import { useAuth } from "../context/AuthContext";
 import { useToast } from "@/context/ToastContext";
 import { useFavorite } from "@/context/FavoriteContext";
 import { useCart } from "@/context/CartContext";
+import { Modal } from "./common/Modal";
 
 
 export interface SaveSelectionModalProps {
@@ -39,13 +39,6 @@ export function SaveSelectionModal({ setIsOpen }: SaveSelectionModalProps) {
     const method = target.checked ? PAYMENT_METHODS.PAYPAL : PAYMENT_METHODS.CASH;
     console.log(method);
     setPaymentMethod(method);
-  }
-
-  const handleClickOut = (event: React.MouseEvent) => {
-    const target = event.target as HTMLDivElement;
-    if (target.id === 'save-modal-backdrop') {
-      setIsOpen(false);
-    }
   }
 
   const saveTo = (destination: string) => {
@@ -87,16 +80,34 @@ export function SaveSelectionModal({ setIsOpen }: SaveSelectionModalProps) {
   }
 
   return (
-    createPortal(
-      <div
-        id="save-modal-backdrop"
-        className="modal-backdrop"
-        onClick={handleClickOut}
-      >
-        <div
-          className="modal-container"
-        >
-          <h3 className="text-center">Dai un nome alla tua poke</h3>
+    <Modal
+      title={`Salva poke`}
+      hideModal={() => setIsOpen(false)}
+      actions={[
+        <ButtonIcon
+          key={`saveFavotires`}
+          icon={<FontAwesomeIcon icon={faStar} />}
+          classes="gold border-r-10"
+          tooltip="Salva nei preferiti"
+          clickHandler={() => saveTo('favorite')}
+        />,
+        <ButtonIcon
+          key={`cancel`}
+          icon={<FontAwesomeIcon icon={faX} />}
+          classes="red border-r-10"
+          tooltip="Annulla"
+          clickHandler={() => setIsOpen(false)}
+        />,
+        <ButtonIcon
+          key={`saveCart`}
+          icon={<FontAwesomeIcon icon={faCartShopping} />}
+          classes="gold border-r-10"
+          tooltip="Salva nel carrello"
+          clickHandler={() => saveTo('cart')}
+        />
+      ]}
+      content={
+        <>
           <input
             type="text"
             placeholder="Nome"
@@ -113,35 +124,8 @@ export function SaveSelectionModal({ setIsOpen }: SaveSelectionModalProps) {
             <label className="custom-toogle pointer" htmlFor="payment-method"></label>
             <label id="paypal" htmlFor="payment-method" className="pointer flex-1 toggle toggle-right text-center disabled">PayPal</label>
           </div>
-
-          <div
-            className="modal-controls flex just-between"
-          >
-
-            <ButtonIcon
-              icon={<FontAwesomeIcon icon={faCartShopping} />}
-              classes="gold border-r-10"
-              tooltip="Salva nel carrello"
-              clickHandler={() => saveTo('cart')}
-            />
-
-            <ButtonIcon
-              icon={<FontAwesomeIcon icon={faX} />}
-              classes="red border-r-10"
-              tooltip="Annulla"
-              clickHandler={() => setIsOpen(false)}
-            />
-
-            <ButtonIcon
-              icon={<FontAwesomeIcon icon={faStar} />}
-              classes="gold border-r-10"
-              tooltip="Salva nei preferiti"
-              clickHandler={() => saveTo('favorite')}
-            />
-          </div>
-        </div>
-      </div>,
-      document.body
-    )
+        </>
+      }
+    />
   )
 }
