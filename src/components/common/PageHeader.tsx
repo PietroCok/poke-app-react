a4562c38cba7309f3e561dafadb5f43f5f008617
PietroCok <pietrocok97@gmail.com>
2025-09-24 23:26:55 +0200

    auto-include cart button if left element is missing

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ButtonIcon } from "./ButtonIcon";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { useCart } from "@/context/CartContext";
 
 export interface PageHeaderProps {
   classes?: string
@@ -7,10 +11,28 @@ export interface PageHeaderProps {
 }
 
 export function PageHeader({ classes, left, center, right }: PageHeaderProps) {
+
+  try {
+    const { getItemsCount } = useCart();
+  
+    if (!left) {
+      left = <ButtonIcon
+        icon={<FontAwesomeIcon icon={faCartShopping} />}
+        classes="gold border-r-10"
+        tooltip="Carrello"
+        linkTo={"/cart"}
+        data-cart-count={getItemsCount() || null}
+      />
+    }
+  } catch (ignored) {
+    // we accept execption here because useCart context maight not be available at this point
+  }
+
   let _classes = classes ? `${classes} ` : '';
 
+
   return (
-    <header 
+    <header
       className={`${_classes}page-header`}
     >
       {renderElement(left, "padding-1")}
