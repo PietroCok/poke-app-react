703f704e2f5bef494bcb7f42fea65aee9295068a
PietroCok <pietrocok97@gmail.com>
2025-09-28 21:51:57 +0200

    improved deploy scripts

--- deploy-prod.cmd
+++ deploy-prod.cmd
@@ -1,19 +1,3 @@
 @echo off
 
-echo -------- Clearing output dir --------
-if exist dist rmdir /s /q dist
-
-echo -------- Building application --------
-call npm run build
-
-echo -------- Publishing --------
-cd dist
-git init
-git add .
-git commit -m "deploy"
-git branch -M gh-pages
-git remote add origin https://github.com/PietroCok/poke-app-react.git
-git push -f origin gh-pages
-
-echo -------- Done --------
-
+call deploy.cmd dist gh-pages build
--- deploy-test.cmd
+++ deploy-test.cmd
@@ -1,19 +1,3 @@
 @echo off
 
-echo -------- Clearing output dir --------
-if exist dist-test rmdir /s /q dist-test
-
-echo -------- Building application --------
-call npm run build-test
-
-echo -------- Publishing --------
-cd dist-test
-git init
-git add .
-git commit -m "deploy-test"
-git branch -M test-env
-git remote add origin https://github.com/PietroCok/poke-app-react.git
-git push -f origin test-env
-
-echo -------- Done --------
-
+call deploy.cmd dist-test test-env build-test
