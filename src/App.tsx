216a742cdb66c6459177ffebda334f863b70808b
PietroCok <pietrocok97@gmail.com>
2026-02-21 14:37:00 +0100

    added dedicated settings page

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import { ToastProvider } from './context/ToastContext';
 import { FavoriteProvider } from './context/FavoriteContext';
 import { Menu } from './pages/Menu';
 import { PokeConfigurator } from './pages/PokeConfigurator';
+import { Settings } from './pages/Settings';
 
 const config: AppConfig = appConfig;
 
@@ -37,6 +38,7 @@ export default function App() {
               <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Registration />} />
+                <Route path="/settings" element={<Settings/>} />
 
                 <Route element={<ProtectedRoute />}>
 
